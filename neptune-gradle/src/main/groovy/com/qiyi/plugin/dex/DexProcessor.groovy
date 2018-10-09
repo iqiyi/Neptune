6a8da93c7d2a066fb99335cffa39b56100df067b
zjupure <zjupure@gmail.com>
2018-10-09 11:17:33 +0800

    fix neptune-gradle Activity class super-call

--- neptune-gradle/src/main/groovy/com/qiyi/plugin/dex/DexProcessor.groovy
+++ neptune-gradle/src/main/groovy/com/qiyi/plugin/dex/DexProcessor.groovy
@@ -85,6 +85,8 @@ class DexProcessor {
         ClassReader cr = new ClassReader(is)
         ClassWriter cw = new ClassWriter(cr, 0)
         ClassVisitor cv = new ClassVisitor(Opcodes.ASM4, cw) {
+            private String targetOwner
+
             @Override
             void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
                 //println "class name => ${name}, superName => ${superName}, interfaces => ${interfaces.toString()}"
@@ -102,9 +104,31 @@ class DexProcessor {
                     String target = ReplaceRules.get(superClsName).replace(".", "/")
                     println "replace class ${name}'s super class name from ${superName} to ${target}"
                     superName = target
+                    targetOwner = target
                 }
 
                 super.visit(version, access, name, signature, superName, interfaces)
+            }
+
+            @Override
+            MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+                MethodVisitor orig = super.visitMethod(access, name, desc, signature, exceptions)
+                if (targetOwner == null) {
+                    // We not replace parent Activity
+                    return orig
+                }
+
+                return new MethodVisitor(Opcodes.ASM4, orig) {
+                    @Override
+                    void visitMethodInsn(int opcode, String owner, String methodName, String methodDesc, boolean itf) {
+                        if (opcode == Opcodes.INVOKESPECIAL) {
+                            // Replace super call
+                            owner = targetOwner
+                        }
+
+                        super.visitMethodInsn(opcode, owner, methodName, methodDesc, itf)
+                    }
+                }
             }
         }
         cr.accept(cv, 0)
